1e6af2e010483e6c17f7ffbf48e58812bba8cd0c
Ivan <vanek777.tv@gmail.com>
2020-09-23 20:04:55 +0300

    Add bottom navigation bar

--- lib/catalog_page.dart
+++ lib/catalog_page.dart
@@ -1,16 +1,55 @@
-import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:staff/my_staff_icons_icons.dart';
 
-class CatalogPage extends StatelessWidget {
+class CatalogPage extends StatefulWidget {
+  @override
+  _CatalogPageState createState() => _CatalogPageState();
+}
+
+class _CatalogPageState extends State<CatalogPage> {
+  int _bottomNavIndex = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: AnimatedBottomNavigationBar(
-        icons: [],
-        activeIndex: 1,
-        notchSmoothness: NotchSmoothness.verySmoothEdge,
-        onTap: (index) {},
+      backgroundColor: Colors.white,
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Color(0xFF272727),
+        unselectedItemColor: Color(0xFF898989),
+        selectedFontSize: 10.0,
+        unselectedFontSize: 10.0,
+        currentIndex: _bottomNavIndex,
+        onTap: (int index) {
+          setState(() {
+            _bottomNavIndex = index;
+          });
+        },
+        items: allDestinations.map((Destination destination) {
+          return BottomNavigationBarItem(
+              icon: Icon(destination.icon),
+              backgroundColor: Color(0xFFEEEEEE),
+              title: Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Text(destination.title),
+              ));
+        }).toList(),
       ),
     );
   }
 }
+
+class Destination {
+  const Destination(this.title, this.icon, this.color);
+  final String title;
+  final IconData icon;
+  final Color color;
+}
+
+const List<Destination> allDestinations = <Destination>[
+  Destination('ОТЗЫВЫ', MyStaffIcons.chat, Color(0xFF272727)),
+  Destination('КАТАЛОГ', MyStaffIcons.cardigan, Color(0xFF272727)),
+  Destination('КОРЗИНА', MyStaffIcons.shopping_cart, Color(0xFF272727)),
+  Destination('ИЗБРАННОЕ', MyStaffIcons.heart, Color(0xFF272727)),
+  Destination('БОЛЬШЕ', MyStaffIcons.spiral, Color(0xFF272727))
+];
