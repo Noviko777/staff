2c0f3915177e68008852b5d19c174781abf868b1
Ivan <vanek777.tv@gmail.com>
2020-09-26 10:31:05 +0300

    Update catalog_page.dart

--- lib/catalog_page.dart
+++ lib/catalog_page.dart
@@ -20,24 +20,93 @@ class _CatalogPageState extends State<CatalogPage>
     //  tabController = TabController(vsync: this, length: 2, initialIndex: 0);
   }
 
-  @override
+  // @override
+  // Widget build(BuildContext context) {
+  //   return DefaultTabController(
+  //     length: 2,
+  //     child: Scaffold(
+  //       backgroundColor: Colors.white,
+  //       body: NestedScrollView(
+  //         controller: controller,
+  //         headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+  //           return <Widget>[
+  //             new SliverAppBar(
+  //               backgroundColor: Colors.white,
+  //               centerTitle: true,
+  //               title: Image.asset('assets/images/logo.png',
+  //                   height: 40, fit: BoxFit.cover),
+  //               pinned: true,
+  //               floating: true,
+  //               forceElevated: innerBoxIsScrolled,
+  //               bottom: new TabBar(
+  //                 labelColor: Color(0xFF222222),
+  //                 indicatorColor: Color(0xFF222222),
+  //                 indicatorWeight: 2.0,
+  //                 tabs: <Tab>[
+  //                   new Tab(text: "СКИДКИ"),
+  //                   new Tab(text: "КАТАЛОГ"),
+  //                 ],
+  //               ),
+  //             )
+  //           ];
+  //         },
+  //         body: TabBarView(
+  //           children: [
+  //             Icon(Icons.directions_car),
+  //             Column(
+  //               mainAxisAlignment: MainAxisAlignment.start,
+  //               children: [
+  //                 Flexible(
+  //                   flex: 1,
+  //                   child: CategoryView()
+  //                 ),
+  //                 Flexible(flex: 13, child: CatalogView(controller)),
+  //               ],
+  //             ),
+  //           ],
+  //         ),
+  //       ),
+  //       bottomNavigationBar: BottomNavigationBar(
+  //         type: BottomNavigationBarType.fixed,
+  //         selectedItemColor: Color(0xFF272727),
+  //         unselectedItemColor: Color(0xFF898989),
+  //         selectedFontSize: 10.0,
+  //         unselectedFontSize: 10.0,
+  //         currentIndex: _bottomNavIndex,
+  //         onTap: (int index) {
+  //           setState(() {
+  //             _bottomNavIndex = index;
+  //           });
+  //         },
+  //         items: allDestinations.map((Destination destination) {
+  //           return BottomNavigationBarItem(
+  //               icon: Icon(destination.icon),
+  //               backgroundColor: Color(0xFFEEEEEE),
+  //               title: Padding(
+  //                 padding: const EdgeInsets.only(top: 8.0),
+  //                 child: Text(destination.title),
+  //               ));
+  //         }).toList(),
+  //       ),
+  //     ),
+  //   );
+  // }
+
+    @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: NestedScrollView(
-          controller: controller,
-          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-            return <Widget>[
-              new SliverAppBar(
+        body: CustomScrollView(
+    slivers: <Widget>[
+       new SliverAppBar(
                 backgroundColor: Colors.white,
                 centerTitle: true,
                 title: Image.asset('assets/images/logo.png',
                     height: 40, fit: BoxFit.cover),
                 pinned: true,
                 floating: true,
-                forceElevated: innerBoxIsScrolled,
                 bottom: new TabBar(
                   labelColor: Color(0xFF222222),
                   indicatorColor: Color(0xFF222222),
@@ -47,16 +116,27 @@ class _CatalogPageState extends State<CatalogPage>
                     new Tab(text: "КАТАЛОГ"),
                   ],
                 ),
-              )
-            ];
-          },
-          body: TabBarView(
-            children: [
-              Icon(Icons.directions_car),
-              CatalogView(controller),
-            ],
+              ),
+              SliverPersistentHeader(
+            delegate: MyDynamicHeader(),
+            pinned: true,
           ),
+      SliverFixedExtentList(
+        itemExtent: 150.0,
+        delegate: SliverChildListDelegate(
+          [
+          
+            Container(color: Colors.red),
+            Container(color: Colors.purple),
+            Container(color: Colors.green),
+            Container(color: Colors.orange),
+            Container(color: Colors.yellow),
+            Container(color: Colors.pink),
+          ],
         ),
+      ),
+    ],
+),
         bottomNavigationBar: BottomNavigationBar(
           type: BottomNavigationBarType.fixed,
           selectedItemColor: Color(0xFF272727),
@@ -83,82 +163,40 @@ class _CatalogPageState extends State<CatalogPage>
     );
   }
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   return DefaultTabController(
-  //     length: 2,
-  //     child: Scaffold(
-  //       backgroundColor: Colors.white,
-  //       body: LoadingMoreCustomScrollView(slivers: [
-  //         new SliverAppBar(
-  //           pinned: true,
-  //           backgroundColor: Colors.white,
-  //           centerTitle: true,
-  //           title: Image.asset('assets/images/logo.png',
-  //               height: 40, fit: BoxFit.cover),
-  //           bottom: new TabBar(
-  //             labelColor: Color(0xFF222222),
-  //             indicatorColor: Color(0xFF222222),
-  //             indicatorWeight: 2.0,
-  //             tabs: <Tab>[
-  //               new Tab(text: "СКИДКИ"),
-  //               new Tab(text: "КАТАЛОГ"),
-  //             ],
-  //           ),
-  //         ),
-  //         SliverPersistentHeader(),
-  //         SliverToBoxAdapter(
-  //           child: Container(
-  //             alignment: Alignment.center,
-  //             child: TabBarView(
-  //               children: [
-  //                 Icon(Icons.directions_car),
-  //                 Icon(Icons.directions_car),
-  //               ],
-  //             ),
-  //             height: 300,
-  //           ),
-  //         ),
-  //         // SliverToBoxAdapter(
-  //         //   child: TabBarView(
-  //         //     children: [
-  //         //       Icon(Icons.directions_car),
-  //         //       Column(
-  //         //         mainAxisAlignment: MainAxisAlignment.start,
-  //         //         children: [
-  //         //           Flexible(flex: 1, child: CategoryView()),
-  //         //           Flexible(flex: 13, child: CatalogView(controller)),
-  //         //         ],
-  //         //       ),
-  //         //     ],
-  //         //   ),
-  //         // )
-  //       ]),
-  //       bottomNavigationBar: BottomNavigationBar(
-  //         type: BottomNavigationBarType.fixed,
-  //         selectedItemColor: Color(0xFF272727),
-  //         unselectedItemColor: Color(0xFF898989),
-  //         selectedFontSize: 10.0,
-  //         unselectedFontSize: 10.0,
-  //         currentIndex: _bottomNavIndex,
-  //         onTap: (int index) {
-  //           setState(() {
-  //             _bottomNavIndex = index;
-  //           });
-  //         },
-  //         items: allDestinations.map((Destination destination) {
-  //           return BottomNavigationBarItem(
-  //               icon: Icon(destination.icon),
-  //               backgroundColor: Color(0xFFEEEEEE),
-  //               title: Padding(
-  //                 padding: const EdgeInsets.only(top: 8.0),
-  //                 child: Text(destination.title),
-  //               ));
-  //         }).toList(),
-  //       ),
-  //     ),
-  //   );
-  // }
+}
+
+class MyDynamicHeader extends SliverPersistentHeaderDelegate {
+  int index = 0;
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return LayoutBuilder(
+        builder: (context, constraints) {
+          final Color color = Colors.primaries[index];
+          final double percentage = (constraints.maxHeight - minExtent)/(maxExtent - minExtent);
+
+          if (++index > Colors.primaries.length-1)
+            index = 0;
+
+          return Container(
+ 
+            height: constraints.maxHeight,
+            child:  CategoryView()
+                
+           
+          );
+        }
+    );
+  }
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate _) => true;
+
+  @override
+  double get maxExtent => 50.0;
+
+  @override
+  double get minExtent => 50.0;
 }
 
 class Destination {
@@ -190,27 +228,16 @@ class CatalogViewState extends State<CatalogView> {
 
   CatalogViewState(this.controller);
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   return ListView.builder(
-  //       itemCount: 100,
-  //       itemBuilder: (context, index) {
-  //         return Container(
-  //           height: 50,
-  //           color: (index % 2 == 0) ? Colors.red : Colors.blue,
-  //         );
-  //       });
-  // }
-
   @override
   Widget build(BuildContext context) {
-    return SliverList(
-        delegate: SliverChildListDelegate([
-      Container(
-        height: 100,
-        color: Colors.green,
-      ),
-    ]));
+    return ListView.builder(
+        itemCount: 100,
+        itemBuilder: (context, index) {
+          return Container(
+            height: 50,
+            color: (index % 2 == 0) ? Colors.red : Colors.blue,
+          );
+        });
   }
 }
 
